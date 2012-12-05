de38b3a1d63a5b7b7f18ed758d062d2615ede722
a2m <yujin1214@a2m.co.kr>
2012-12-05 13:25:53 +0900

    Removed iconv dependency
    Use String's force_encoding instead of iconv

--- lib/korean_zipcode_finder.rb
+++ lib/korean_zipcode_finder.rb
@@ -6,7 +6,6 @@ require 'engine' if defined?(Rails)
 
 require 'net/http'
 require 'open-uri'
-require 'iconv'
 require 'nokogiri'
 
 
@@ -15,8 +14,7 @@ module KoreanZipcodeFinder
   Struct.new("KoreanZipcodeFinder", "zipcode", "zipcode_01", "zipcode_02", "address", "original_address")
   
   def self.find_zipcode(dong_name)
-    iconv = Iconv.new("euc-kr", "utf-8//IGNORE")
-    keyword = iconv.conv(dong_name.strip)
+    keyword = dong_name.force_encoding('euc-kr')
 
     response = Net::HTTP.post_form URI.parse(Configuration::URL), {'regkey' => api_key, 'target' => 'post', 'query' => keyword}
     nodes = Nokogiri::XML(response.body).css("item")
